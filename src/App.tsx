3709ad44247586503a7c054ed15a21166ddd9133
Illia <ilja.cherkasov@gmail.com>
2024-04-25 08:11:30 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="planner">
+        <h1>Event Planner</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
